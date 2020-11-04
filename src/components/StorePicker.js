6872f1de805ba930a0e5eef1ce2610dab7aa2f80
Emilia <emilia.mena26@gmail.com>
2020-11-04 03:20:34 -0500

    setting up first basic component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+//same as import { Component } from 'react';
+import React from 'react';
+
+//same as - extendes Component
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hello!</p>
+    }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+//same as import { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+
+//takes two things - first JSX, second mounting point
+render(<StorePicker />, document.querySelector('#main'));
+
