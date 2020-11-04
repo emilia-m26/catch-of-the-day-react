e893f735cbc36fee3218b0f16992d9f7c8011646
Emilia <emilia.mena26@gmail.com>
2020-11-04 12:30:26 -0500

    css file imported

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-
+import "./css/style.css";
 
 
 //takes two things - first JSX, second mounting point
