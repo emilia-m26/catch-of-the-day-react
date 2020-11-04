a7057afeeadde43a7ac29838d511d9c5afdff288
Emilia <emilia.mena26@gmail.com>
2020-11-04 12:55:06 -0500

    adding components that I'm planning to use

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import "./css/style.css";
 
 
 //takes two things - first JSX, second mounting point
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
 
