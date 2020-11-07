189889593c00ef4f81d21a67c33e1f4ef43694c2
Emilia <emilia.mena26@gmail.com>
2020-11-06 20:32:14 -0500

    minor adjs

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,7 @@ class StorePicker extends React.Component {
     render() {
         return (
         <form className="store-selector">
-            <h2>Please Enter A Store</h2>
+            <h2>Please Enter A Store Name</h2>
             <input type="text" required placeholder="Store Name"></input>
             <button type="submit"> Visit Store ➡ </button>
         </form>
