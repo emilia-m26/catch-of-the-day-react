842e3ecafbda87954c43a6f3b13a577d7a682f5b
Emilia <emilia.mena26@gmail.com>
2020-11-08 19:10:44 -0500

    working on making store picker functional

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,26 @@ import React from 'react';
 
 //same as - extendes Component
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    myInput = React.createRef();
+    goToStore = (event) => {
+         //stop form from submitting
+        event.preventDefault();
+        //get text from input
+        console.log(this.myInput);
+        //change page to /store/whatever-input
+        
+    }
+
     render() {
         return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter A Store Name</h2>
-            <input type="text" required placeholder="Store Name"></input>
+            <input type="text" ref={this.myInput} required placeholder="Store Name"></input>
             <button type="submit"> Visit Store ➡ </button>
         </form>
         )
