ef31ff785b9431f36c09ebec588395ba788974a4
Emilia <emilia.mena26@gmail.com>
2020-11-09 12:29:38 -0500

    working on form to add fish

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <p>it works!</p>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
