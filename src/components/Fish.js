3ab848e7073780168b6a49ffe6cd9a5ed89015a4
Emilia <emilia.mena26@gmail.com>
2020-11-11 17:23:10 -0500

    format pricing

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
     render() {
         // const image = this.props.detials.image;
         // const name =this.props.details.name;
-        const {image, name} = this.props.details; //destructuring
+        const { image, name, price, descr, status } = this.props.details; //destructuring
 
         return (
             <li className="menu-fish">
                 <img src={image} alt={name} />
-                <h3 className="fish-name">{name}</h3>
+                <h3 className="fish-name">
+                    {name}
+        <span className="price">{formatPrice(price)}</span>
+                </h3>
             </li>
         )
     }
