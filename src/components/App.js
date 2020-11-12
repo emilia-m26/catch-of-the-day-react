9667ee83667ced26fabfbe89d4f189354c66469b
Emilia <emilia.mena26@gmail.com>
2020-11-12 16:33:38 -0500

    adding to order, next step is to display on page

--- src/components/App.js
+++ src/components/App.js
@@ -45,7 +45,7 @@ class App extends React.Component {
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
                     </ul>
                 </div>
                 <Order />
