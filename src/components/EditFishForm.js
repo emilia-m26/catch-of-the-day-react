e74f57f336d4d6242977cd0ce5599e0360eed455
Emilia <emilia.mena26@gmail.com>
2020-11-14 15:58:15 -0500

    edit form showing and values populating

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class EditFishForm extends React.Component {
     render(){
-        return <p>Edit Fish</p>
+        return (
+        <div className="fish-edit">
+            <input name="name" type="text" value={this.props.fish.name} />
+                <input name="price" type="text" value={this.props.fish.price} />
+                <select name="status" value={this.props.fish.status} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc"value={this.props.fish.desc} ></textarea>
+                <input name="image" type="text" value={this.props.fish.image} />
+                <button type="submit">+ Edit Fish</button>
+        </div>
+        );
     }
 }
 
