2e7b0b3662e31904bb51f4a0dae9cd6e13b065be
Emilia <emilia.mena26@gmail.com>
2020-11-14 14:34:29 -0500

    basejs config added:

--- src/base.js
+++ src/base.js
@@ -3,4 +3,8 @@ import Rebase from 're-base';
 //official firebase package
 import firebase from 'firebase';
 
-const firebaseApp = firebase.initializeApp()
+const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyBI_q8qfIMenC6Wh4ONW3zql3PTNPp1NMs",
+        authDomain: "catch-of-the-day-fresh-market.firebaseapp.com",
+        databaseURL: "https://catch-of-the-day-fresh-market.firebaseio.com"        
+});
