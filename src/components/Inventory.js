bb22b92b1235fc0d3b1d4394e3eb9dc1dba83170
Emilia <emilia.mena26@gmail.com>
2020-11-14 16:14:02 -0500

    fixing error, passing to edit fish form not add fish form

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,8 +7,8 @@ class Inventory extends React.Component {
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
-                {Object.keys(this.props.fishes).map(key => <EditFishForm key={key} fish={this.props.fishes[key]}/>)}
-                <AddFishForm addFish={this.props.addFish} updateFish={this.props.updateFish} />
+                {Object.keys(this.props.fishes).map(key => <EditFishForm key={key} fish={this.props.fishes[key] updateFish={this.props.updateFish}} />)}
+                <AddFishForm addFish={this.props.addFish} />
                 <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
         )
